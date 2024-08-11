55d0ced431962ef9a6508a3922f68cd5219c0649
do hoai nho <dohoainho@192.168.5.35>
2024-08-11 14:24:26 +0700

    Initial commit

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Do Hoai Nho!");
+        System.out.println("Welcome to DHKTPM19A!");
+    }
+}
